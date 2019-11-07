338797e0daa9f50f84c3634e4923fe67a039f807
suidaniel <k783242002@gmail.com>
2019-11-07 21:57:46 +0800

    first change

---
+++ app/src/main/java/com/dragongold/test2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dragongold.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String s = "for test222222";
+        int t = 777;
+    }
+}
